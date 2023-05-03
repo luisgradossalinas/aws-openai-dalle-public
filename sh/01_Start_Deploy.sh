d9b434046ec80c985e97b4cff08d73b339312f90
Luis Grados Salinas <martin.programmer@gmail.com>
2023-05-02 20:25:22 -0500

    Update 01_Start_Deploy.sh

--- sh/01_Start_Deploy.sh
+++ sh/01_Start_Deploy.sh
@@ -8,7 +8,6 @@ aws s3 mb s3://${PROJECT}-code-${ACCOUNT_ID}
 sleep 5
 
 aws s3 cp lambda/awsdalle.zip s3://${PROJECT}-code-${ACCOUNT_ID}/lambda/
-
 pip3 install -r web/requirements.txt
 
 aws cloudformation create-stack --stack-name StackDalle --template-body file://dalle.yaml --capabilities CAPABILITY_NAMED_IAM
